16a21233570e8222a72dd29c335465b8014b9cc0
Patrick Cavalcante <54637677+PatrickCavalcant@users.noreply.github.com>
2022-03-30 15:25:25 -0300

    Arquivo JSON

---
+++ JSON.js
@@ -0,0 +1,37 @@
+{
+   "cFilialCp":"010103",
+   "cCodigoCp":"000021",
+   "cCodCli":"999803391",
+   "cLojaCli":"0001",
+   "cNomeCli":"ADRIANA DA SILVA SANTOS",
+   "cDddCli":"63",
+   "cTelCli":"92445362",
+   "cEmailCli":"adriana.santos@meridionalmaqui",
+   "cNivelEmp":"01",
+   "cTemperat":"1",
+   "cOpc":"2",
+   "item1":{
+      "cFilGd":"010103",
+      "cCodGd":"000004",
+      "cCodMarc":"JD",
+      "cModVei":"CA 1550 - 5500",
+      "cQnt":"3",
+      "cOperFab":"TESTANDO",
+      "cDateInt":"202220331",
+      "cDateLimt":"202220331",
+      "cCodVend":"000000",
+      "cTipoCon":"2"
+   },
+      "item2":{
+      "cFilGd":"010103",
+      "cCodGd":"000004",
+      "cCodMarc":"JD",
+      "cModVei":"CA 1550 - 5500",
+      "cQnt":"2",
+      "cOperFab":"TESTE ALTERACAO",
+      "cDateInt":"202220331",
+      "cDateLimt":"202220331",
+      "cCodVend":"000000",
+      "cTipoCon":"1"
+   }
+}
